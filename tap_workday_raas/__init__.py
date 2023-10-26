a0069808a4c4910cf00156e9b4ad47fe0dd4efac
jay-varicent <jama@varicent.com>
2023-10-26 11:42:12 -0400

    WP-17438 err log fix

--- tap_workday_raas/__init__.py
+++ tap_workday_raas/__init__.py
@@ -12,6 +12,9 @@ from tap_workday_raas.sync import sync_report
 REQUIRED_CONFIG_KEYS = ["username", "password", "reports"]
 LOGGER = singer.get_logger()
 
+# for symon error logging
+ERROR_START_MARKER = '[tap_error_start]'
+ERROR_END_MARKER = '[tap_error_end]'
 
 def do_discover(config):
     LOGGER.info("Starting discover")
@@ -84,21 +87,23 @@ def main():
         raise
     finally:
         if error_info is not None:
-            error_file_path = args.config.get('error_file_path', None)
-            if error_file_path is not None:
-                try:
-                    with open(error_file_path, 'w', encoding='utf-8') as fp:
-                        json.dump(error_info, fp)
-                except:
-                    pass
-            # log error info as well in case file is corrupted
-            error_info_json = json.dumps(error_info)
-            error_start_marker = args.config.get(
-                'error_start_marker', '[tap_error_start]')
-            error_end_marker = args.config.get(
-                'error_end_marker', '[tap_error_end]')
-            LOGGER.info(
-                f'{error_start_marker}{error_info_json}{error_end_marker}')
+            try:
+                error_file_path = args.config.get('error_file_path', None)
+                if error_file_path is not None:
+                    try:
+                        with open(error_file_path, 'w', encoding='utf-8') as fp:
+                            json.dump(error_info, fp)
+                    except:
+                        pass
+                # log error info as well in case file is corrupted
+                error_info_json = json.dumps(error_info)
+                error_start_marker = args.config.get('error_start_marker', ERROR_START_MARKER)
+                error_end_marker = args.config.get('error_end_marker', ERROR_END_MARKER)
+                LOGGER.info(f'{error_start_marker}{error_info_json}{error_end_marker}')
+            except:
+                # error occurred before args was parsed correctly, log the error
+                error_info_json = json.dumps(error_info)
+                LOGGER.info(f'{ERROR_START_MARKER}{error_info_json}{ERROR_END_MARKER}')
 
 
 if __name__ == "__main__":
